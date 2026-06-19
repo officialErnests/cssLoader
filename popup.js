d8690a0a183c69742031c6fc2158d5016914f874
officialErnests <ernestspursw@gmail.com>
2026-06-19 15:07:47 +0300

    Presets

--- popup.js
+++ popup.js
@@ -1,13 +1,26 @@
 const textare = document.getElementById("css")
+const curTab = document.getElementById("current")
 async function load_def() {
-    const web = await browser.storage.local.get("curent_web");
-    const key = "web_" + web.curent_web;
+    let hostname = "default"
+    await browser.tabs.query({ active: true, currentWindow: true })
+    .then(tabs => {
+      const currentTab = tabs[0];
+      hostname = new URL(currentTab.url).hostname
+    });
+    const key = "web_" + hostname;
     const result = await browser.storage.local.get(key);
     textare.value = result[key];
+    curTab.innerText = hostname;
 }
+load_def()
 document.getElementById("save").addEventListener("click", async () => {
-    const web = await browser.storage.local.get("curent_web");
-    const key = "web_" + web.curent_web;
+    let hostname = "default"
+    await browser.tabs.query({ active: true, currentWindow: true })
+    .then(tabs => {
+      const currentTab = tabs[0];
+      hostname = new URL(currentTab.url).hostname
+    });
+    const key = "web_" + hostname;
     await browser.storage.local.set({[key]: textare.value});
 });
 document.getElementById("reload").addEventListener('click', async () => {
